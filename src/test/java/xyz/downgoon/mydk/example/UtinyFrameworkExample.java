9d28f317f1d90e90ec0a4051e9fd384374596050
downgoon <downgoon@qq.com>
2017-10-20 08:47:42 +0800

    RESTful api demo for UtinyFramework

--- src/test/java/xyz/downgoon/mydk/example/UtinyFrameworkExample.java
+++ src/test/java/xyz/downgoon/mydk/example/UtinyFrameworkExample.java
@@ -3,6 +3,9 @@ package xyz.downgoon.mydk.example;
 import java.util.ArrayList;
 import java.util.List;
 
+import xyz.downgoon.mydk.framework.Command;
+import xyz.downgoon.mydk.framework.CommandException;
+import xyz.downgoon.mydk.framework.CommandHandler;
 import xyz.downgoon.mydk.framework.utiny.EchoHandler;
 import xyz.downgoon.mydk.framework.utiny.NamedTraceStopableFilter;
 import xyz.downgoon.mydk.framework.utiny.UtinyCommand;
@@ -12,7 +15,7 @@ public class UtinyFrameworkExample {
 
 	public static void main(String[] args) {
 		UtinyFramework framework = new UtinyFramework();
-		
+
 		// filters
 		List<String> tracer = new ArrayList<>();
 		NamedTraceStopableFilter logFilter = new NamedTraceStopableFilter("logFilter", false, tracer);
@@ -22,13 +25,48 @@ public class UtinyFrameworkExample {
 		framework.location("/**", null, logFilter);
 		framework.location("/fastapi/**", null, timerFilter);
 		framework.location("/fastapi/echo", new EchoHandler(tracer));
-		
-		
+
+		// RESTful style: Resource&Action - EntPoint
+		UserHandler user = new UserHandler();
+		framework.location("/user/**", null, (cmd, chain) -> {
+			System.out.println("-> /user RESTful api ..." + cmd.path());
+			chain.doFilter(cmd);
+			System.out.println("<- /user RESTful api ... Fin");
+		});
+		framework.location("/user/create", user::create);
+		framework.location("/user/view", user::view); // Java8 Function Pointer
+		framework.location("/user/remove", new CommandHandler() {
+			
+			@Override
+			public void exec(Command command) throws CommandException {
+				user.remove(command); // adaptor pattern before Java8
+			}
+		});
+
 		// handle command
 		UtinyCommand command = new UtinyCommand("/fastapi/echo");
-		
 		framework.exec(command);
-		
+
+		framework.exec(new UtinyCommand("/user/create"));
+		framework.exec(new UtinyCommand("/user/view"));
+		framework.exec(new UtinyCommand("/user/remove"));
+
+	}
+
+	static class UserHandler {
+
+		public void create(Command command) throws CommandException {
+			System.out.println("user create action");
+		}
+
+		public void view(Command command) throws CommandException {
+			System.out.println("user view action");
+		}
+
+		public void remove(Command command) throws CommandException {
+			System.out.println("user remove action");
+		}
+
 	}
 
 }
